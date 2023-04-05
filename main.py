a36a441aa4979960a4b3eb26a84d4444cb3ef3ad
Zinovev Evgeniy <zinovev.evgenii@mail.ru>
2023-04-05 07:47:03 +0300

    rial_date

--- main.py
+++ main.py
@@ -64,8 +64,8 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
-    approve_choice: str = None
-    char_class: str = None
+    approve_choice: str = ''
+    char_class: str = ''
     while approve_choice != 'y':
         char_class: str = input('Введи название персонажа, '
                                 'за которого хочешь играть: Воитель — '
@@ -103,10 +103,10 @@ def choice_char_class() -> str:
 def main() -> str:
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
+    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
